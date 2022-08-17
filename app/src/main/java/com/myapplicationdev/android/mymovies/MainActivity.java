75cc02eecdc9c77a7cdef58d8b76fe3e328aa4a2
Starguardian09 <21021646@rp.edu.sg>
2022-08-17 15:43:53 +0800

    Done

--- app/src/main/java/com/myapplicationdev/android/mymovies/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/mymovies/MainActivity.java
@@ -1,8 +1,6 @@
 package com.myapplicationdev.android.mymovies;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
