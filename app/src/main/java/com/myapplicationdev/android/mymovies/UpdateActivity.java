3a36a7b61e939146be763d9609351e5b330e8900
Starguardian09 <21021646@myrp.edu.sg>
2022-08-17 15:54:36 +0800

    Done

--- app/src/main/java/com/myapplicationdev/android/mymovies/UpdateActivity.java
+++ app/src/main/java/com/myapplicationdev/android/mymovies/UpdateActivity.java
@@ -23,7 +23,6 @@ public class UpdateActivity extends AppCompatActivity {
     Button btnUpdate, btnDelete, btnCancel;
     Movie data;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
